960735af31f0d8f1b6e337776a9f2cfa13e2c5dd
musoye <oyebamijimustapha44@gmail.com>
2024-06-28 15:06:43 +0100

    feat: add handler for retrieving a note

--- api.go
+++ api.go
@@ -53,6 +53,22 @@ func GetNoteHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(j)
 }
 
+func GetParticularNoteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	k := vars["id"]
+	if note, ok := noteStore[k]; ok {
+		j, err := json.Marshal(note)
+		if err != nil {
+			panic(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
 func PutNoteHandler(w http.ResponseWriter, r *http.Request) {
 	var err error
 	vars := mux.Vars(r)
@@ -90,6 +106,7 @@ func main() {
 	r := mux.NewRouter().StrictSlash(false)
 	r.HandleFunc("/api/notes", GetNoteHandler).Methods("GET")
 	r.HandleFunc("/api/notes", PostNoteHandler).Methods("POST")
+	r.HandleFunc("/api/notes/{id}", GetParticularNoteHandler).Methods("GET")
 	r.HandleFunc("/api/notes/{id}", PutNoteHandler).Methods("PUT")
 	r.HandleFunc("/api/notes/{id}", DeleteNoteHandler).Methods("DELETE")
 	server := &http.Server{
